f8a00e24f712c1169db21938aadc1ed5b34fa484
kunalsoni <kunalsoni3331@gmail.com>
2019-10-25 20:01:52 +0530

    implement audio player with slider

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { RouteGuardService } from './core/services/app-route-guard.service';
 const routes: Routes = [
   { path: '', canActivate: [RouteGuardService], loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'login', loadChildren: './auth/login/login.module#LoginPageModule' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+
 ];
 @NgModule({
   imports: [
